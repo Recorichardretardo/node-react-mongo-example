e7b9114e97730945415771c6b8350f65f0963daf
kkasiviswanath367 <kkasiviswanath367@gmail.com>
2021-03-12 03:11:44 +0530

    added user post service method

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
 const express = require('express');
 require('dotenv').config();
-
+const connectDB = require('./config/db');
 
 const app = express();
+
+// Connect Database
+connectDB();
+
+// Init Middleware
+// bodyParser.json
+
+app.use(express.json({ extended: false }));
+
+// Define Routes
+// http://localhost:8080/api/users/test
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
 
 app.get('/', (req,res) => res.send('API Running'));
 
